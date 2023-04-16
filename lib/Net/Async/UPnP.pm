1869f07d250289204083b840dad8e7d3aa8cfa23
Max Maischein <corion@corion.net>
2023-04-16 20:49:34 +0200

    Use BSD license just as Net::UPnP does

--- lib/Net/Async/UPnP.pm
+++ lib/Net/Async/UPnP.pm
@@ -27,3 +27,36 @@ sub entity_decode( $r ) {
 }
 
 1;
+
+=head1 SEE ALSO
+
+L<Net::UPnP> - the module which this code is based on
+
+=head1 REPOSITORY
+
+The public repository of this module is
+L<https://github.com/Corion/net-async-upnp>.
+
+=head1 SUPPORT
+
+The public support forum of this module is L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the Github bug queue at
+L<https://github.com/Corion/Net-Async-UPnP/issues>
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2010-2023 by Max Maischein C<corion@cpan.org>.
+Based on Code by Satoshi Konno Copyright 2005-2018.
+
+=head1 LICENSE
+
+It may be used, redistributed, and/or modified under the terms of BSD License.
+
+=cut
